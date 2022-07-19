3d0104da8e4eb02400b1faaa4a3445e1ec04f8ed
kocak-ilyas <kocakilyas18@gmail.com>
2022-07-19 18:58:45 -0400

    Contact and router add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link, Route, Routes } from "react-router-dom";
 import { showCreateModal } from "./actions";
 
 import Products from "./components/Products";
 import Product from "./components/Product";
 import Create from "./components/Create";
+import Contact from "./components/Contact";
 
 import { Container, Navbar } from "react-bootstrap";
-import { FcPlus } from "react-icons/fc";
+import { FcPlus, FcBusinessContact } from "react-icons/fc";
 import "./App.scss";
 
 function App() {
@@ -20,30 +22,45 @@ function App() {
   };
   return (
     <div className='App'>
-      <div className='navbarContainer shadow pl-3 pr-3 mb-5 bg-body rounded'>
-        <Navbar>
-          <Container fluid className='mx-5'>
-            <Navbar.Brand href='/'>UPayments Store</Navbar.Brand>
-            <Navbar.Collapse className='justify-content-end'>
-              <Navbar.Text style={{ marginRight: `50px`, cursor: "pointer" }} onClick={() => handleShowCreateModal()}>
-                <FcPlus size={25} />
-                &nbsp;&nbsp;Add Product
-              </Navbar.Text>
-              <Navbar.Text>Register</Navbar.Text>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </div>
-      <Products />
-      <div className='container'>
-        <footer className='d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top'>
-          <div className='col-md-4 d-flex align-items-center'>
-            <span className='text-muted'>&copy; 2022 UPayments</span>
-          </div>
-        </footer>
-      </div>
-      {isCreateModal && <Create />}
-      {isProductModal && <Product />}
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+              <div className='navbarContainer shadow pl-3 pr-3 mb-5 bg-body rounded'>
+                <Navbar>
+                  <Container fluid className='mx-5'>
+                    <Navbar.Brand href='/'>UPayments Store</Navbar.Brand>
+                    <Navbar.Collapse className='justify-content-end'>
+                      <Navbar.Text
+                        style={{ marginRight: `50px`, cursor: "pointer" }}
+                        onClick={() => handleShowCreateModal()}>
+                        <FcPlus size={25} />
+                        &nbsp;&nbsp;Add Product
+                      </Navbar.Text>
+                      <Link to='/contact'>
+                        <FcBusinessContact size={35} />
+                        &nbsp;Contact with Author
+                      </Link>
+                    </Navbar.Collapse>
+                  </Container>
+                </Navbar>
+              </div>
+              <Products />
+              <div className='container'>
+                <footer className='d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top'>
+                  <div className='col-md-4 d-flex align-items-center'>
+                    <span className='text-muted'>&copy; 2022 UPayments</span>
+                  </div>
+                </footer>
+              </div>
+              {isCreateModal && <Create />}
+              {isProductModal && <Product />}
+            </div>
+          }
+        />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,77 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Table } from "react-bootstrap";
+import { FcHome } from "react-icons/fc";
 
 const Contact = () => {
   return (
     <div>
-      <h1>contact</h1>
-      <button>Close</button>
+      <div
+        className='card d-flex flex-wrap justify-content-between align-items-center'
+        style={{ width: `49%`, margin: "15px auto" }}>
+        <div className='card-header'>
+          <Link to='/'>
+            <FcHome size={17} />
+            &nbsp;&nbsp;Home
+          </Link>
+        </div>
+        <div
+          className='card-body flex-wrap justify-content-between align-items-center py-3 my-4'
+          style={{ textAlign: "left" }}>
+          <Table style={{ textAlign: `left` }}>
+            <tbody>
+              <tr>
+                <td>
+                  <strong>Repo</strong>
+                </td>
+                <td>
+                  <a href='https://github.com/kocak-ilyas/UPaymentsTask' className='card-link'>
+                    https://github.com/kocak-ilyas/UPaymentsTask
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Demo</strong>
+                </td>
+                <td>
+                  <a href='https://upayment.netlify.app/' className='card-link'>
+                    https://upayment.netlify.app
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Linkedin</strong>
+                </td>
+                <td>
+                  <a href='https://www.linkedin.com/in/ilyaskocak' className='card-link'>
+                    https://www.linkedin.com/in/ilyaskocak
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>GitHub</strong>
+                </td>
+                <td>
+                  <a href='https://github.com/kocak-ilyas' className='card-link'>
+                    https://github.com/kocak-ilyas
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong>Gmail</strong>
+                </td>
+                <td>
+                  <span className='card-link'>kocakilyas18@gmail.com</span>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      </div>
     </div>
   );
 };
